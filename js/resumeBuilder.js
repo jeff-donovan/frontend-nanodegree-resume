aa7408e9c839ec791ae02b5ff981f2d310e811d8
Jeff Donovan <jeffcdonovan@gmail.com>
2015-01-15 16:12:45 -0800

    Add name and role to resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,8 @@
+var name = "Jeffrey C Donovan";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
